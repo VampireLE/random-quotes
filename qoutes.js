4d762d52eff97f18dd3e10aaffd53adc3da3ca07
georgy.kostin <egor_kostin_1999@mail.ru>
2025-05-21 18:46:00 +0300

    Splitting app into the js modules

---
+++ qoutes.js
@@ -0,0 +1,44 @@
+const quotes = [
+    {
+        quote: 'The only way to do great work is to love what you do.',
+        author: 'Steve Jobs'
+    },
+    {
+        quote: 'Innovation distinguishes between a leader and a follower.',
+        author: 'Steve Jobs'
+    },
+    {
+        quote: "Your time is limited, so don't waste it living someone else's life.",
+        author: 'Steve Jobs'
+    },
+    {
+        quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        author: 'Winston Churchill'
+    },
+    {
+        quote: "Life is what happens when you're busy making other plans.",
+        author: 'John Lennon'
+    },
+    {
+        quote: "Imagination is more important than knowledge.",
+        author: 'Albert Einstein'
+    },
+    {
+        quote: "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+        author: 'Ralph Waldo Emerson'
+    },
+    {
+        quote: "In the middle of every difficulty lies opportunity.",
+        author: 'Albert Einstein'
+    },
+    {
+        quote: "Whether you think you can or you think you can’t, you’re right.",
+        author: 'Henry Ford'
+    },
+    {
+        quote: "I have not failed. I've just found 10,000 ways that won't work.",
+        author: 'Thomas Edison'
+    }
+];
+
+export default quotes;
