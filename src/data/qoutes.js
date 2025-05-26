74427d8c9b5c3731c5aa52c8f375443474dffc63
georgy.kostin <egor_kostin_1999@mail.ru>
2025-05-26 21:55:21 +0300

    Added unique ids to the array of quotes

--- src/data/qoutes.js
+++ src/data/qoutes.js
@@ -1,54 +1,54 @@
 const quotes = [
     {
-        text: 'The only way to do great work is to love what you do.',
-        author: 'Steve Jobs',
-        isFavorite: false
+      id: 'c718475e',
+      text: 'The only way to do great work is to love what you do',
+      author: 'Steve Jobs',
     },
     {
-        text: 'Innovation distinguishes between a leader and a follower.',
-        author: 'Steve Jobs',
-        isFavorite: false
+      id: '3452d8aa',
+      text: 'Innovation distinguishes between a leader and a follower',
+      author: 'Steve Jobs',
     },
     {
-        text: "Your time is limited, so don't waste it living someone else's life.",
-        author: 'Steve Jobs',
-        isFavorite: false
+      id: '56487b49',
+      text: "Your time is limited, so don't waste it living someone else's life",
+      author: 'Steve Jobs',
     },
     {
-        text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-        author: 'Winston Churchill',
-        isFavorite: false
+      id: '1e3736f8',
+      text: 'Strive not to be a success, but rather to be of value',
+      author: 'Albert Einstein',
     },
     {
-        text: "Life is what happens when you're busy making other plans.",
-        author: 'John Lennon',
-        isFavorite: false
+      id: '2520d4da',
+      text: 'The only thing we have to fear is fear itself',
+      author: 'Franklin D. Roosevelt',
     },
     {
-        text: "Imagination is more important than knowledge.",
-        author: 'Albert Einstein',
-        isFavorite: false
+      id: '0446d47e',
+      text: 'Life is what happens when you’re busy making other plans',
+      author: 'John Lennon',
     },
     {
-        text: "Do not go where the path may lead, go instead where there is no path and leave a trail.",
-        author: 'Ralph Waldo Emerson',
-        isFavorite: false
+      id: 'ea08d995',
+      text: 'Success is not final, failure is not fatal: It is the courage to continue that counts',
+      author: 'Winston Churchill',
     },
     {
-        text: "In the middle of every difficulty lies opportunity.",
-        author: 'Albert Einstein',
-        isFavorite: false
+      id: '9ac6bee8',
+      text: 'The greatest glory in living lies not in never falling, but in rising every time we fall',
+      author: 'Nelson Mandela',
     },
     {
-        text: "Whether you think you can or you think you can’t, you’re right.",
-        author: 'Henry Ford',
-        isFavorite: false
+      id: '05370aeb',
+      text: 'The future belongs to those who believe in the beauty of their dreams',
+      author: 'Eleanor Roosevelt',
     },
     {
-        text: "I have not failed. I've just found 10,000 ways that won't work.",
-        author: 'Thomas Edison',
-        isFavorite: false
-    }
-];
-
-export default quotes;
+      id: '423c35bb',
+      text: 'You miss 100% of the shots you don’t take',
+      author: 'Wayne Gretzky',
+    },
+  ];
+  
+  export default quotes;
